d435e3c62458f9f29039d1db05f303cdbf7e56ec
Md Warej Ali <waraj.com@gmail.com>
2022-05-07 14:18:57 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,42 +10,74 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorize access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+    
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rdu4c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const productCollection = client.db('warehouse-management').collection('products');
-        
-        
-        app.get('/product', async (req, res)=>{
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken });
+        })
+
+        //API for Product
+        app.get('/product', async (req, res) => {
             const query = {}
-        const cursor = productCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products)
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
         });
 
-        app.get('/product/:id', async(req, res)=>{
+        app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product)
         })
 
-        app.post('/product', async(req, res)=>{
+        app.post('/product', async (req, res) => {
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         })
 
         // API for My items
-        app.get('/items', async(req, res)=>{
+        app.get('/items', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
             const email = req.query.email;
-            const query = {email: email};
-            const cursor = productCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items)
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = productCollection.find(query);
+                const items = await cursor.toArray();
+                res.send(items)
+            }
+            else{
+                res.status(403).send({message: 'Forbidden access'})
+            }
         })
 
         // update product
@@ -52,8 +85,8 @@ async function run(){
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
             const updateProduct = req.body;
-            const filter = {_id: ObjectId(id)};
-            const options = {upsert: true};
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
             const updateDoc = {
                 $set: {
                     name: updateProduct.name,
@@ -69,15 +102,15 @@ async function run(){
         })
 
         // Delete API
-        app.delete('/product/:id', async(req, res)=>{
+        app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await productCollection.deleteOne(query)
             res.send(result)
         })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -90,6 +123,6 @@ app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Lishing port', port);
 })
